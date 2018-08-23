3c8878e4cceca1ad52d9712db1134762da1f6e48
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-08-23 11:15:36 -0500

    Kelvin to Celsius

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -6,7 +6,7 @@ import GoogleMap from '../components/GoogleMap';
 class WeatherList extends Component {
     renderWeather = (cityData) =>{
         const name = cityData.city.name
-        const temps = cityData.list.map(weather => weather.main.temp)
+        const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273)
         //_.map((...), (temp)=> temp -273(TALVEZ))
         const pressure = cityData.list.map(weather => weather.main.pressure)
         const humidity = cityData.list.map(weather => weather.main.humidity)
@@ -16,9 +16,9 @@ class WeatherList extends Component {
         return (
             <tr key={name}>
                 <td><GoogleMap lon={lon} lat={lat} /></td>
-                <td><Chart data={temps} color="orange" unity="K"/></td>
+                <td><Chart data={temps} color="orange" unity="°C"/></td>
                 <td><Chart data={pressure} color="green" unity="hPA"/></td>
-                <td><Chart data={humidity} color="black" unity="%"/></td>
+                <td><Chart data={humidity} color="blue" unity="%"/></td>
             </tr>
         )
     }
@@ -28,7 +28,7 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature (K)</th>
+                        <th>Temperature (°C)</th>
                         <th>Pressure (hPA)</th>
                         <th>Humidity (%)</th>
                     </tr>
