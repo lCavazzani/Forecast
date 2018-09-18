8895519a8e00770d9f8f17ff55dabb982f3ce7db
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-09-18 14:27:21 -0500

    https

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const API_KEY = 'f85ed366ae74931fb4c5ab8176794cfb';
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
-export const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`
+export const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`
 
 export function fetchWeather(city){
     const url = `${ROOT_URL}&q=${city},us`
